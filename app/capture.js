2f1c59198b8392d3f3f460acefcb4cf2c7a8b383
Yanin Tuamsuk <khame.yanawaro@gmail.com>
2015-07-29 09:35:04 +0700

    add scroll page

--- app/capture.js
+++ app/capture.js
@@ -7,6 +7,17 @@ var filename = "./app/screenshots/" + (argv.name || "__") + ".png";
 new Nightmare()
   .viewport( (argv.w || 1200), (argv.h || 900))
   .goto( (argv.url || "http://www.google.com") )
+  .scrollTo(200, 0)
+  .wait(500)
+  .scrollTo(500, 0)
+  .wait(500)
+  .scrollTo(1000, 0)
+  .wait(500)
+  .scrollTo(1500, 0)
+  .wait(500)
+  .scrollTo(2000, 0)
+  .wait(2000)
+  .scrollTo 0, 0
   .screenshot(filename)
   .run(function (err, nightmare) {
     fs.readFile(filename, function (err, data) {
