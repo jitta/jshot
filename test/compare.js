545488ec35c638b510009dae3b9826291c2e9bb1
Yanin Tuamsuk <khame@jitta.com>
2016-02-24 12:44:07 +0700

    fix tests to use before hook

--- test/compare.js
+++ test/compare.js
@@ -4,23 +4,21 @@
 
 describe('Running comparing screenshots demo test', function () {
 
-  it('Example app has started', function (done) {
-    
-    var app = require('../example-app/app.js'); //have your app instance fired up first.
-    app.on('listening', done);
-    
-    /*
-      Intregrate these into your mocha test files.
-    */
-    var jshot = require('../index.js');
-    
-    //setup config and environment.
-    var config = require('./config.js');
-    config.env = 'local';
-
-    //fired up to generate screenshots.
-    jshot.compare(config);
-
+  var app = undefined;
+  
+  before( function (done) {  
+      app = require('../example-app/app.js');  //have your app instance fired up first.
+      app.on('listening', done);
   });
+  
+  //Intregrate these into your mocha test files.
+  var jshot = require('../index.js');
 
+  //setup config and environment.
+  var config = require('./config.js');
+  config.env = 'local';
+  
+  //fired up to compare screenshots.
+  jshot.compare(config);
+  
 });
